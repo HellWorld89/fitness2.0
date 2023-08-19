d61ccb1a901e4fb253abec5c87594c78f05ee0e4
Vladimir <sapozhnikovl@yandex.ru>
2023-08-19 17:09:57 +0800

    Добавляет секцию тренажерный зал

---
+++ source/js/modules/play-video.js
@@ -0,0 +1,14 @@
+const playButton = document.querySelector('.gym__video-button');
+const video = document.querySelector('.gym__video');
+
+function playVid() {
+  video.play();
+}
+
+function pauseVid() {
+  video.pause();
+}
+
+// playButton.addEventListener('click', () => {
+//   video.play();
+// });
